e92f55c1a9a674ca4e565a0c8a238cbd220ac4d1
Harper Reed <harper@nata2.org>
2018-04-07 15:28:29 -0500

    updated and added some feedback

--- images_to_pptx.py
+++ images_to_pptx.py
@@ -19,38 +19,42 @@ try:
 except ImportError:
     flags = None
 
-loglevel = logging.CRITICAL
+loglevel = logging.INFO
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=loglevel)
 logger = logging.getLogger(__name__)
 
 class presentation_generator:
 
-    def __init__(self, presentation_name, slides_path, root_path="./", width=1920, height=1080):
+    def __init__(self, presentation_name, slides_path, root_path="./", slide_pattern="slide*", width=1920, height=1080):
         self.presentation_name = presentation_name
-        self.slug = slugify(self.presentation_name)
+        self.slug = slugify(unicode(self.presentation_name))
         self.width = width
         self.height = height
         self.slides_path = slides_path
         self.root_path = root_path
+        self.slide_pattern = slide_pattern
 
     def build(self):
         # new
+        logging.info("Generating PPTX")
         prs = pptx.Presentation()
  
         prs.slide_height = Pt(self.height)
         prs.slide_width = Pt(self.width)
- 
-        slides = natsorted(glob.glob(self.slides_path + "slide_*"))
-        print slides
 
+
+        logging.info("Grabbing slides")
+        slides = natsorted(glob.glob(self.slides_path + self.slide_pattern))
+
+        logging.info("Adding slides")
         for slide_image in slides:
             slide = prs.slides.add_slide(prs.slide_layouts[6])
             pic   = slide.shapes.add_picture(slide_image, Pt(0), Pt(0))
 
 
         filename = self.root_path + self.slug + "_-" + str(self.width) + "x" + str(self.height) + ".pptx"
- 
+        logging.info("Saving PPTX: " + filename)
         prs.save(filename)
         
 
@@ -67,6 +71,7 @@ def main():
     slides_path = flags.slides_path
 
     presentation = presentation_generator(presentation_name=name, root_path=root_path, slides_path=slides_path )
+
     presentation.build()
 
 
